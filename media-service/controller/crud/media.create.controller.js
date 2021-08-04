e4b2dd1c46a347af5113d2897d125062bbd764cb
Ritesh Thakur <riteshkumar3310@gmail.com>
2021-08-04 13:42:39 +0530

    made some changes

--- media-service/controller/crud/media.create.controller.js
+++ media-service/controller/crud/media.create.controller.js
@@ -1,22 +1,16 @@
 const MediaModel = require("../../../Database/Media.Service.DB/mediaSchema");
-const MediaPartnerModel = require("../../../Database/MediaPartner.Service.DB/mediaPartnerSchema");
-const AdminModel = require("../../../Database/Admin.Service.DB/adminSchema");
 const { mongooseErrorHandler } = require("../../../Database/Error/DB.Error");
 
 exports.mediaCreate = async (req, res) => {
   const mediaData = req.body;
   try {
     // console.log(mediaData);
-    // const saveMedia = await MediaModel.create(mediaData);
-    // console.log(saveMedia);
-    // const pushMedia = await MediaPartnerModel.updateOne(
-    //   { _id: saveMedia.Owner },
-    //   { $push: { Media: saveMedia._id } }
-    // );
-    const findMediaOwner = await MediaModel.findOne({
-      _id: "610a461accffc3277e07aa05",
-    }).populate("Owner", "Name Email", AdminModel);
-    console.log(findMediaOwner.Owner);
+    const saveMedia = await MediaModel.create(mediaData);
+    console.log(saveMedia);
+    // const findMediaOwner = await MediaModel.findOne({
+    //   _id: "610a461accffc3277e07aa05",
+    // }).populate("Owner", "Name Email", AdminModel);
+    // console.log(findMediaOwner.Owner);
     // const findMedia = await MediaModel.findOne({
     //   _id: findMediaOwner.Owner.Media,
     // });
